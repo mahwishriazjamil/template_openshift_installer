48053da18c163a989335eb1a449117a01c6939bb
Mahwish Riaz-Jamil <32422134+mahwishriazjamil@users.noreply.github.com>
2020-11-23 11:15:55 +0000

    Update main.tf

--- aws/terraform/modules/setup-install/main.tf
+++ aws/terraform/modules/setup-install/main.tf
@@ -28,7 +28,7 @@ resource "random_id" "setup_install" {
     host        = "${var.bastion_public_ip}"
     user        = "${var.rhel_user}"
     agent       = false
-    timeout     = "5m"
+    timeout     = "90m"
     private_key = "${var.vm_private_key}"
   }
 
